411df65d71a3db8eb823971c336108657fcad94c
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-06-09 12:41:28 +0200

    Add button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
     cellAlign: 'center',
-    wrapAround: true,
+//    wrapAround: true,
     contain: true,
     hash: true,
 });
@@ -17,3 +17,12 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+var wrappedButton = document.querySelector('.button--next-wrapped');
+
+wrappedButton.addEventListener( 'click', function() {
+  flkty.next( true );
+});
+
+var template = document.getElementById('template');
+
