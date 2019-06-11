970dfc56b9eb8a70d444bdff5868200484f3767d
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-06-11 18:41:47 +0200

    Add button

--- js/script.js
+++ js/script.js
@@ -1,35 +1,43 @@
-var templateList = document.getElementById('template-cell').innerHTML;
+(function() {
+  var templateList = document.getElementById("template-cell").innerHTML;
  
-Mustache.parse(templateList);
+  Mustache.parse(templateList);
  
-var listItems = '';
-
-for(var i = 0; i < objects.length; i++){
-    listItems += Mustache.render(templateList, objects[i])
-};
-    
-var results = document.getElementById('main-carousel'); 
-
-results.insertAdjacentHTML('beforeend', listItems);
-
-
-var elem = document.querySelector('#main-carousel');
-var flkty = new Flickity( elem, {
-    cellAlign: 'center',
+  var listItems = "";
+  for (var i = 0; i < objects.length; i++) {
+    listItems += Mustache.render(templateList, objects[i]);
+  }
+  var results = document.getElementById("main-carrousel");
+  results.insertAdjacentHTML("beforeend", listItems);
+  var elem = document.querySelector("#main-carrousel");
+  var flkty = new Flickity(elem, {
+    cellAlign: "center",
     wrapAround: true,
     contain: true,
-    hash: true,
+    hash: true
+  });
+ 
+  var flkty = new Flickity("#main-carrousel", {});
+ 
+  var flkty = new Flickity("#main-carrousel");
+ 
+  var progressBar = document.querySelector(".progress-bar");
+ 
+  flkty.on("scroll", function(progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + "%";
+  });
+    
+    var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray(buttons);
+
+buttonGroup.addEventListener('click', function (event) {
+    // filter for button clicks
+    if (!matchesSelector(event.target, '.button')) {
+        return;
+    }
+    var index = buttons.indexOf(event.target);
+    flkty.select(index);
 });
-
-var flkty = new Flickity( '.main-carousel', {
-});
-
-var flkty = new Flickity('.main-carousel');
-
-var progressBar = document.querySelector('.progress-bar')
-
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
-});
-
+})();
