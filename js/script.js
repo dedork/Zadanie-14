2d85113c8e2420f1489404984a48280868f1003c
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-06-20 22:10:06 +0200

    Add markers

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ var buttons = buttonGroup.querySelectorAll('.button');
 buttons = fizzyUIUtils.makeArray(buttons);
 
 buttonGroup.addEventListener('click', function (event) {
-    // filter for button clicks
+     
     if (!matchesSelector(event.target, '.button')) {
         return;
     }
@@ -41,3 +41,91 @@ buttonGroup.addEventListener('click', function (event) {
     flkty.select(index);
 });
 })();
+
+//Google maps
+
+(function(){
+    window.initMap = function(){
+        var infos = document.getElementById('infos');
+        
+        var map = new google.maps.Map(document.getElementById('map'), 
+        {
+            zoom: 4,
+            center: objects[0].coords
+        });
+        
+        
+//        var markerOne = new google.maps.Marker({
+//            position: objects[0].coords,
+//            map: map
+//        });
+//        
+//        markerOne.addListener('click', function(){
+//            infos.innerHTML = 'You are in mountains in Canada';
+//        });
+//        
+//        var markerTwo = new google.maps.Marker({
+//			position: objects[1].coords,
+//			map: map
+//		});
+//        
+//        markerTwo.addListener('click', function(){
+//            infos.innerHTML = 'You are in Alps in Austria';
+//        });
+//        
+//        var markerThree = new google.maps.Marker({
+//			position: objects[2].coords,
+//			map: map
+//		});
+//		
+//		markerThree.addListener('click', function(){
+//			infos.innerHTML = 'You are in old town in Japan';
+//		});	
+//        
+//        var markerFour = new google.maps.Marker({
+//			position: objects[3].coords,
+//			map: map
+//		});
+//		
+//		markerThree.addListener('click', function(){
+//			infos.innerHTML = 'You are by some lake in Norway';
+//		});	 
+//        
+//        var markerFive = new google.maps.Marker({
+//			position: objects[4].coords,
+//			map: map
+//		});
+//		
+//		markerThree.addListener('click', function(){
+//			infos.innerHTML = 'You are in the pasture in Zakopane';
+//		});
+        
+        for (var i = 0; i < objects.length; i++) {
+        var markers = new google.maps.Marker({
+			position: objects[i].coords,
+			map: map
+		  });
+        }
+    }    
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
