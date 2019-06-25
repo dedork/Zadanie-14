a06aaa129955a84d9c34bdc89e89429b4e949f17
Rafal Kwiatkowski <rafalkwiatkowski8@gmail.com>
2019-06-25 22:05:51 +0200

    Clean code

--- js/script.js
+++ js/script.js
@@ -9,60 +9,61 @@ var marker = [];
 var results = document.getElementById("main-carrousel");
 
 (function() {
-  Mustache.parse(templateList);
-    var listItems = "";
-    for (var i = 0; i < objects.length; i++) {
-    listItems += Mustache.render(templateList, objects[i]);
-  }
+    Mustache.parse(templateList);
+        var listItems = "";
+        for (var i = 0; i < objects.length; i++) {
+        listItems += Mustache.render(templateList, objects[i]);
+    };
     
     results.insertAdjacentHTML("beforeend", listItems);
     var elem = document.querySelector("#main-carrousel");
-    var flkty = new Flickity(elem, {
+    var flkty = new Flickity(elem, 
+    {
     cellAlign: "center",
     wrapAround: true,
     contain: true,
     hash: true
-  });
+    });
 
-  flkty.on('scroll', function(progress) {
-    progress = Math.max(0, Math.min(1, progress));
-    progressBar.style.width = progress * 100 + "%";
-  });
+    flkty.on('scroll', function(progress) {
+        progress = Math.max(0, Math.min(1, progress));
+        progressBar.style.width = progress * 100 + "%";
+    });
 
     buttons = fizzyUIUtils.makeArray(buttons);
     buttonGroup.addEventListener('click', function(event) {
-    var index = buttons.indexOf(event.target);
-    flkty.select(index);
-  });
+        var index = buttons.indexOf(event.target);
+        flkty.select(index);
+    });
 
     var flkty = new Flickity("#main-carrousel");
 
     window.initMap = function() {
-    var map = new google.maps.Map(document.getElementById("map"), 
-    {
-    zoom: 4,
-    center: objects[0].coords
-  });
+        var map = new google.maps.Map(document.getElementById("map"), 
+        {
+        zoom: 4,
+        center: objects[0].coords
+        });
 
     for (let i = 0; i < objects.length; i++) {
-    marker[i] = new google.maps.Marker({
-      position: objects[i].coords,
-      map: map
+        marker[i] = new google.maps.Marker({
+        position: objects[i].coords,
+        map: map
         });
         
-    marker[i].addListener("click", function() {
-      flkty.select(i);
-    });
+        marker[i].addListener("click", function() {
+            flkty.select(i);
+        });
         
-    flkty.on('change', function (index){
-        map.panTo(objects[i].coords);
-        map.setZoom(5),
-        {
-        center: objects[i].coords,
-        }
-    });     
+        flkty.on('change', function (index){
+            map.panTo(objects[i].coords);
+            map.setZoom(5),
+            {
+            center: objects[i].coords,
+            }
+        });     
         
-};
+    };
 
 }
 
